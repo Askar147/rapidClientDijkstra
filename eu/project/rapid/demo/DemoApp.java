2a87fa9ad7a7a4dbaecebfa085f66780c5d049a2
pico_lab_pc <pico@lab.pc>
2024-03-11 14:48:17 +0600

    Implement only reading input data from saved files. Remove socket connection transfer

--- eu/project/rapid/demo/DemoApp.java
+++ eu/project/rapid/demo/DemoApp.java
@@ -21,6 +21,12 @@ import java.util.Arrays;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
 public class DemoApp {
     private DFE dfe;
 
@@ -320,107 +326,54 @@ public class DemoApp {
 
         int port = 12345;
         DemoApp demo = new DemoApp(vmIP, connType);
-        try (ServerSocket serverSocket = new ServerSocket(port, 50, InetAddress.getByName("0.0.0.0"))) {
-            System.out.println("Server is listening on port " + port);
-            while (true) {
-                try (Socket socket = serverSocket.accept();
-                     InputStream inputStream = socket.getInputStream();
-                     DataInputStream input = new DataInputStream(inputStream);
-                     DataOutputStream output = new DataOutputStream(socket.getOutputStream())) {
-
-                    // Reading double parameters
-                    byte[] doubleBytes = new byte[8];
-                    input.readFully(doubleBytes);
-                    double start_x = bytesToDouble(doubleBytes);
-
-                    input.readFully(doubleBytes);
-                    double start_y = bytesToDouble(doubleBytes);
-
-                    input.readFully(doubleBytes);
-                    double end_x = bytesToDouble(doubleBytes);
-
-                    input.readFully(doubleBytes);
-                    double end_y = bytesToDouble(doubleBytes);
-
-                    // Reading int parameters: cycles and size of charMap
-                    int cycles = input.readInt();
-                    int size = input.readInt();
-
-                    System.out.println("Received start_x: " + start_x);
-                    System.out.println("Received start_y: " + start_y);
-                    System.out.println("Received end_x: " + end_x);
-                    System.out.println("Received end_y: " + end_y);
-                    System.out.println("Received cycles: " + cycles);
-                    System.out.println("Received charMap size: " + size);
-
-                    // Read charMap data
-                    byte[] charMap = new byte[size];
-                    input.readFully(charMap);
-                    //System.out.println("Received charMap data (partial): " + Arrays.toString(Arrays.copyOf(charMap, 10)));
-
-                    //TODO: Change potential to float everywhere
-                    byte[] costs = charMap; // Placeholder, replace with actual data
-                    float[] potential = new float[0]; // Placeholder, replace with actual data
-                    // Call the constructor with the parameters received from the client
+        //TODO: REMOVE potential array from method calls
+        float[] potential = new float[1];
+        int index = 0;
+        while (true) {
 
 
-                    demo.testDijkstraLogs(costs, start_x, start_y, end_x, end_y, cycles, potential);
+                // READING
+            int numClientData = 200;
+            Random random = new Random();
+            int randomClientIndex = random.nextInt(numClientData);
 
 
-//                    //SENDING RESULTS(POTENTIALS ARRAY AND BOOLEAN) BACK TO CPP
-//                    ByteBuffer buffer = ByteBuffer.allocate(demo.result.potential.length * Float.BYTES);
-//                    buffer.order(ByteOrder.BIG_ENDIAN); // Ensure big-endian to match network byte order
-//                    for (float value : demo.result.potential) {
-//                        buffer.putFloat(value);
-//                    }
-
-                    // Send foundLegal
-                    output.writeBoolean(demo.result.result);
-
-//                    // Send the size of potentialArray
-//                    output.writeInt(buffer.array().length);
-//
-//                    // Send potentialArray as byte array
-//                    output.write(buffer.array());
-
-
-                    //SENDING RESULTS IN TUPLE(POTENTIALS AS FLOAT AND INDEX AS INT)
-                    // Send the length of the tuple array
-                    try {
-                        output.writeInt(demo.result.newPotential.size());
-                    } catch (NullPointerException ex){
-                        ex.printStackTrace();
-                    }
-
-
-                    // Serialize and send each tuple
-                    ByteBuffer bufferTuple = ByteBuffer.allocate(8);
-
-
-
-                    for (int i = 0; i < demo.result.newPotential.size(); i++) {
-                        ((Buffer)bufferTuple).clear();
-                        bufferTuple.putFloat(demo.result.newPotential.get(i).floatValue());
-                        bufferTuple.putInt(demo.result.newPotentialIndex.get(i).intValue());
-//                        System.out.println("Tuple: (" +demo.result.newPotential.get(i).floatValue() + ", (" + demo.result.newPotentialIndex.get(i).intValue() + ")");
-                        output.write(bufferTuple.array());
-                    }
-
-
-
-                    output.flush();
-                    // For demonstration, sending back a simple acknowledgment
-                    output.writeInt(1); // Acknowledgment size
-                    output.writeByte(1); // Acknowledgment byte
-
+            byte[] readCosts = new byte[416 * 160]; // The same size as the original costs array
+            String inputCostsDir = "/home/user/Desktop/Costs/costs" + randomClientIndex + ".dat";
+            String inputParamDir = "/home/user/Desktop/Costs/costs_param" + randomClientIndex + ".dat";
+                try (FileInputStream fis = new FileInputStream(inputCostsDir);
+                     BufferedInputStream bis = new BufferedInputStream(fis)) {
+                    int totalBytesRead = bis.read(readCosts);
+                    System.out.println("Read " + totalBytesRead + " bytes from costs.dat");
                 } catch (IOException e) {
-                    System.out.println("Exception in client communication: " + e.getMessage());
+                    System.out.println("An error occurred while reading costs from file: " + e.getMessage());
                     e.printStackTrace();
                 }
-            }
-        } catch (IOException ex) {
-            System.out.println("Server exception: " + ex.getMessage());
-            ex.printStackTrace();
+
+                double start_x_FromFile, start_y_FromFile, end_x_FromFile, end_y_FromFile;
+                int cycles_FromFile;
+
+                try (FileInputStream fis = new FileInputStream(inputParamDir);
+                     DataInputStream dis = new DataInputStream(fis)) {
+                    // Reading the parameters from file
+                    start_x_FromFile = dis.readDouble();
+                    start_y_FromFile = dis.readDouble();
+                    end_x_FromFile = dis.readDouble();
+                    end_y_FromFile = dis.readDouble();
+                    cycles_FromFile = dis.readInt();
+                    System.out.println("Read start_x: " + start_x_FromFile);
+                    System.out.println("Read start_y: " + start_y_FromFile);
+                    System.out.println("Read end_x: " + end_x_FromFile);
+                    System.out.println("Read end_y: " + end_y_FromFile);
+                    System.out.println("Read cycles: " + cycles_FromFile);
+
+                    //FIXME: this is just testDijkstraLogs for reading (xz kak eshe), krch drugoi testDijkstraLogs ne nado uzat kogda etot est
+                    demo.testDijkstraLogs(readCosts, start_x_FromFile, start_y_FromFile, end_x_FromFile, end_y_FromFile, cycles_FromFile, potential);
+
+                } catch (IOException e) {
+                    System.err.println("IOException while reading parameters from file: " + e.getMessage());
+                    e.printStackTrace();
+                }
         }
         demo.dfe.destroy();
         log.warn("DEMOAPP: OOPS! I'm dead:(");
